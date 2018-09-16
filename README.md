bca6c7729fabad4adf7fb90863726898cb135ede
mattcam <matt@mattcameron.me>
2018-09-16 21:35:21 +1000

    init

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # inf-client-samrentsch
+
+> share data analysis
+
+Motivation for creating app is to learn about share data analysis.
+
+## Prerequisites
+
+Node
+
+## Usage
+
+```
+npm install
+npm run start
+```
+---
+
+Please feel free to suggest improvements on [issues](https://github.com/mattcam/tfjs_xor_example/issues)
---
+++ app.js
@@ -0,0 +1,37 @@
+var _ = require('underscore');
+var async = require('async');
+var csv = require('csvtojson');
+
+var stocks=[];
+var results=[];
+
+async.series([
+
+  function(_callback) {
+
+    async function go() {
+      stocks=await csv().fromFile('data/all_stocks_5yr.csv');
+      _callback(null,'app-read-csv');
+    }
+    go();
+  },
+
+  function(_callback) {
+    var rejects = _.filter(stocks, function(item) { return item.close < 200 })
+    var rejectedNames = _.uniq(rejects, function(item) { return item.Name; });
+    results = _.reject(stocks,function(item){return _.find(rejectedNames,{Name: item.Name});});
+    _callback(null,'app-debug');
+  },
+
+  function(_callback) {
+    var acceptedNames = _.uniq(results, function(item) { return item.Name; });
+    console.log('acceptedNames ',acceptedNames);
+    console.log('acceptedNames.length ',acceptedNames.length);
+    console.log('results.length ',results.length);
+    _callback(null,'');
+  },
+
+],
+function(err, results) {
+  console.log(results);
+});
